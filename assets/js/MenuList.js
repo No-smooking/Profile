9bd873ec51a1547487c746af1326857ed24a52f7
unknown <tuan080895@gmail.com>
2022-11-10 18:11:23 +0700

    up load file

---
+++ assets/js/MenuList.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.icon-list').click(function(){
+        $('.sub-nav').toggle();
+    })
+
+    $('.sub-nav').click(function(){
+        $('.sub-nav').hide()
+    })
+    
+
+})
